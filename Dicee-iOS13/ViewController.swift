4969f7b149e25b17b66826ecfc441f6c2abd4dbb
Rupaj Sen <rupajps4@gmail.com>
2022-02-06 12:45:23 +0530

    linking design to code

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Dicee-iOS13
 //
-//  Created by Angela Yu on 11/06/2019.
+//  Created by Rupaj Sen on 06/02/22
 //  Copyright © 2019 London App Brewery. All rights reserved.
 //
 
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //IBOutlet allows us to refer to an UI Element
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // who    what     value
+        
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView1.alpha = 0.5
+        
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        
     }
 
 
